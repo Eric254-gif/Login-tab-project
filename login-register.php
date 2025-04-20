c1707fcce6923355d91303946cad9ca28367875c
eric musembi <musembieric43@gmail.com>
2025-04-21 01:04:23 +0300

    initial commit

--- login-register.php
+++ login-register.php
@@ -26,10 +26,11 @@ exit();
 if(isset($_POST['login'])){
   $email=$_POST['email'];
   $password=$_POST['password'];
-  $result=$conn->query("SELECT *FROM users WHERE email='$email'");
+
+  $result=$conn->query("SELECT * FROM users WHERE email='$email'");
   if($result->num_rows >0){
     $user=$result->fetch_assoc();
-    if(password_verify($password,$user['password'])){
+    if(password_verify($password, $user['password'])){
       $_SESSION['name'] = $user['name'];
       $_SESSION['email'] = $user['email'];
 
